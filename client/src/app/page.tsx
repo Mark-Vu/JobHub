020addb5ac6a1431d7467ba5db6e0e99db2f576a
Mark Vu <markvu2003@gmail.com>
2023-10-21 19:11:56 -0700

    added front end layout

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,51 @@
-import Image from 'next/image';
+import { useState } from 'react';
+import logo from '../../.next/image/logo.png'; // Import the image
+import "./homepage.css"
 
-export default function Home() {
-	return <main className="JobHubPage">abc</main>;
+export default function Homepage() {
+	const [option, setOption] = useState("job")
+	function handleSelectOption() {
+		
+	}
+  return (
+    <div className="grid-container">
+        <header>
+            <h1>Job Hub</h1>
+            <div className='header-buttons'>
+                <button className='outline'>Filter</button>
+                <button className='fill'>Sign in</button>
+            </div>
+        </header>
+        <div className="main-content">
+            <div className="content">
+                <table>
+                    <thead>
+                        <tr>
+                            <th><img className='logo' src={logo.src} alt="Logo" /></th>
+                            <th>Company</th>
+                            <th>Position</th>
+                            <th>Location</th>
+                            <th>Info</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><button>Job</button></td>
+                            <td>Ragged Edge</td>
+                            <td>Account Manager</td>
+                            <td>London</td>
+                            <td>+</td>
+                        </tr>
+                        <tr>
+                            <td><button>Refresh</button></td>
+                        </tr>
+                        <tr>
+                            <td><button>About</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+  );
 }
